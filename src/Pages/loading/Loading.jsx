83938ab5cd509199f877e444b46080735a27f44b
Jonatan Delgado <delgadojonatan.b@gmail.com>
2023-02-17 13:34:29 +0100

    loading animation css

--- src/Pages/loading/Loading.jsx
+++ src/Pages/loading/Loading.jsx
@@ -1,7 +1,17 @@
 import React from "react";
-
+import style from "./loading.module.scss";
 const Loading = () => {
-  return <div>Loading...</div>;
+  return (
+    <main className={style.backgroundPage}>
+      <div className={style.animationContanier}>
+        <img
+          className={style.animationContanier__image}
+          src="logos/logo.png"
+          alt="logo web"
+        />
+      </div>
+    </main>
+  );
 };
 
 export default Loading;
