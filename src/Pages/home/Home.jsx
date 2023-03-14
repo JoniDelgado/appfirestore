2c17101e46e9c7dd85d7589ba1d92fa33c049b71
Jonatan Delgado <delgadojonatan.b@gmail.com>
2023-03-14 11:59:42 +0100

    probando base de datos de usuario

--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -1,12 +1,56 @@
+import style from "./home.module.scss";
+import { ref, set, onValue } from "firebase/database";
+import { database } from "../../firebase/firebase";
+import { useEffect, useState } from "react";
 import { useUser } from "../../hooks/useUser";
 
 const Home = () => {
   const user = useUser();
+  // const [data, setData] = useState([]);
+
+  useEffect(() => {
+    if (user) {
+      const userRef = ref(database, `${user.uid}/`);
+      onValue(userRef, (snapshot) => {
+        const data = snapshot.val();
+        if (!data) {
+          set(ref(database, `${user.uid}/tasks/`), {
+            id: 0,
+            task: "no hay tareas asignadas por el momento",
+          });
+        }
+      });
+    }
+  }, []);
+
+  const handleWrite = () => {
+    console.log(data);
+    // set(ref(database, "users/" + userId), {
+    //   username,
+    //   email,
+    //   img,
+    // });
+  };
 
   return (
-    <div>
+    <section>
       <h1>Este es el home</h1>
-    </div>
+      <div className={style.projectContainer}>
+        <img
+          className={style.projectContainer__image}
+          src="https://cdn.pixabay.com/photo/2017/09/12/11/56/universe-2742113_1280.jpg"
+          alt="imagen del espacio"
+        />
+        <p className={style.projectContainer__emoji}>😂</p>
+        <div className={style.nameProject}>
+          <p>Nuevo proyecto</p>
+        </div>
+      </div>
+
+      <button onClick={() => handleWrite()}>escribir dato</button>
+
+      <div></div>
+    </section>
   );
 };
 
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { GoogleAuthProvider } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -10,10 +10,12 @@ const firebaseConfig = {
   storageBucket: "multitaskapp-51651.appspot.com",
   messagingSenderId: "15603279607",
   appId: "1:15603279607:web:8d7b3ebf757542ae9395f2",
+  databaseURL: "https://multitaskapp-51651-default-rtdb.firebaseio.com/",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+export const database = getDatabase(app);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider(app);
